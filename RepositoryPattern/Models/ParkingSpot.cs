82c6667aa258095493d77cfdb8956e8fdc9cb2dd
Ajaypal Singh <122804961+Ajeypalsingh@users.noreply.github.com>
2023-10-13 00:46:05 -0500

    model added

---
+++ RepositoryPattern/Models/ParkingSpot.cs
@@ -0,0 +1,9 @@
+﻿namespace RepositoryPattern.Models
+{
+    public class ParkingSpot
+    {
+        public int ID { get; set; }
+        public bool Occupied { get; set; }
+        public virtual ICollection<Reservation> Reservations { get; set; }
+    }
+}
